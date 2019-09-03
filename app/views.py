265b2de075d9b7e5cd0b89471f0271b2b01bdd13
ombaejr <ombaejr@gmail.com>
2019-09-03 07:18:24 +0300

    Write view classmethod for login or sign up

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import PostComment,PostImageForm,PostProfile
+from .models import Images, Profiles, Comments
 
 # Create your views here.
+@login_required(login_url='/accounts/register/')
+def index(request):
+    images = Images.get_all_images()
