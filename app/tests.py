87d272b97892960d4ef765a372d4dc7edb7cf78e
ombaejr <ombaejr@gmail.com>
2019-09-03 08:23:32 +0300

    Create the image test class

--- app/tests.py
+++ app/tests.py
@@ -32,3 +32,44 @@ class ProfileTestClass(TestCase):
 
     def tearDown(self):
         Image.objects.all().delete()
+
+class ImageTestClass(TestCase):
+
+    def setUp(self):
+        self.user= User(username='hello',email='hello@gmail.com', password='hello123')
+        self.user.save()
+
+        self.profile=Profiles(images='/posts',bio='kenya love',user=self.user)
+        self.profile.save()
+
+        self.image = Images(image='posts/',caption='testing2',pub_date='28/07/2019',profile=self.user,posted=auto_now())
+        self.image.save_image()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Images))
+
+    def test_save_image(self):
+        self.image.save_image()
+        images= Images.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_update_caption(self):
+        self.image.save_image()
+        kwargs={'image':'/posts','caption':'dont stop me now'}
+        Image.update_caption(self.image.id,**kwargs)
+        self.assertEqual("dont stop me now",self.image.caption)
+
+
+    def test_delete_image(self):
+        self.image.save_image()
+        self.image.delete_image()
+        images = Images.objects.all()
+        self.assertTrue(len(images) == 0)
+
+    def test_get_image_id(self):
+        image_id = id
+        self.image.objects.get(pk=id)
+        self.assertTrue(pk=id)
+
+    def tearDown(self):
+        Images.objects.all().delete()
